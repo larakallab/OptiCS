861844a16276cd906a33dae5d3aa90671e9cc4e9
AnonymUsername <47190340+AnonymUsername@users.noreply.github.com>
2020-06-04 19:03:51 +0200

    Update MVD2.py

--- Resources Descriptions in OpenCEMS/MVD2.py
+++ Resources Descriptions in OpenCEMS/MVD2.py
@@ -21,13 +21,13 @@ def getDescriptorValDetection():
 			"@id": "http://51.77.148.187:5001/resourceDescription/missvaldet.md",
 			"entrypoint": "http://51.77.148.187:5001/resource/missvaldet",
 			"location": "",
-			"image": "mvd.png",
 			"Qres": [{"Dynamicity" : "0"},{"Availability" : "95"}],
 			"Operation": [{
 				"method": "GET",
 				"expects": ["tabValues"],
 				"returns": ["tabTimestamp"],
-				"functionality": "MVD", 
+				"functionality": "MVD",
+				"image": "mvd.png",
 				"Qf": [{"Cost" : "0"},{"Usage": "10"}]
 				}],
 			"Link": [{
