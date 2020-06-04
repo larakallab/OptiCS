031a6cde1ad98f86bfac84c7349366e0938cdb38
AnonymUsername <47190340+AnonymUsername@users.noreply.github.com>
2020-06-04 18:46:31 +0200

    Update ATC1.py

--- Resources Descriptions in OpenCEMS/ATC1.py
+++ Resources Descriptions in OpenCEMS/ATC1.py
@@ -21,6 +21,7 @@ def getDescriptorAirTemperature():
 			"@id": "http://51.77.148.187:5001/resourceDescription/getAirTemperature.md",
 			"entrypoint": "http://51.77.148.187:5001/resource/getairtemperature",
 			"location": "Zone1",
+			"image": "atc.png",
 			"Qres": [{"Dynamicity" : "0"},{"Availability" : "70"}],
 			"Operation": [{
 				"method": "GET",
